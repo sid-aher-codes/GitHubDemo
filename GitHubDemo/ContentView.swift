ed0a12859b92543c78bff73be65cd037fd186396
Sid Aher <sidaher14@gmail.com>
2023-04-15 08:14:13 -0400

    Added hello world

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Sid'd world!")
+            Text("Hello, Sid'd world !")
         }
         .padding()
     }
